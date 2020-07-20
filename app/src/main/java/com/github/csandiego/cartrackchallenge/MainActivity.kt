91ea198cc5fb2482279e5d144403ec75351457e8
Christopher San Diego <csandiego@gmail.com>
2020-07-20 21:04:14 +0800

    Add country list

--- app/src/main/java/com/github/csandiego/cartrackchallenge/MainActivity.kt
+++ app/src/main/java/com/github/csandiego/cartrackchallenge/MainActivity.kt
@@ -2,10 +2,17 @@ package com.github.csandiego.cartrackchallenge
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ArrayAdapter
+import android.widget.AutoCompleteTextView
+import com.github.csandiego.cartrackchallenge.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        val binding = ActivityMainBinding.inflate(layoutInflater)
+        val countries = resources.getStringArray(R.array.countries)
+        val adapter = ArrayAdapter(this, R.layout.list_item, countries)
+        (binding.countryTextInputLayout.editText as AutoCompleteTextView).setAdapter(adapter)
+        setContentView(binding.root)
     }
 }
